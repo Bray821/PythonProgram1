2cb232982e448bc00ba4e852f88e95713f263e60
Bray821 <68251170+Bray821@users.noreply.github.com>
2020-10-06 18:33:53 -0600

    LearningPython

--- Program3.py
+++ Program3.py
@@ -1,1 +1,54 @@
-print("Hello World")
+li = ["Improve","Problem","Solving","Skills"]
+
+
+
+
+
+
+li.sort()
+
+
+
+
+print(li)
+
+print("These tips are very good")
+
+
+li.reverse()
+
+print(li)
+
+
+print(len(li))
+
+wonderOftheWorld = ["Great Wall of China","Christ The Reedemer","Machu Picchu","Chichen Itza","Roman Colosseum","Taj Mahal","Petra"]
+
+print(len(wonderOftheWorld))
+
+
+print(f"This is the wonders of the world if it was sorted in alphabitcal order temporarly {sorted(wonderOftheWorld)}")
+
+
+print(wonderOftheWorld)
+print(f"This is what the it would look like if it was printed in reverse alphabetical order {sorted(wonderOftheWorld, reverse=True)}")
+
+wonderOftheWorld.reverse()
+
+print(wonderOftheWorld)
+
+wonderOftheWorld.reverse()
+
+print(wonderOftheWorld)
+
+wonderOftheWorld.sort()
+
+print(wonderOftheWorld)
+
+wonderOftheWorld.sort(reverse=True)
+
+print(wonderOftheWorld)
+
+wonderOftheWorld.sort()
+
+print(wonderOftheWorld)
